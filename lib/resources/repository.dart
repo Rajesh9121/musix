54fb206dcc5b70485f7ae496dd65d38ab9e50a12
Rajesh Alajangi <66308071+Rajesh9121@users.noreply.github.com>
2022-07-21 19:27:45 +0530

    Musix App made with bloc pattern

---
+++ lib/resources/repository.dart
@@ -0,0 +1,12 @@
+import 'dart:async';
+import 'package:musix/models/lyrics.dart';
+
+import 'trending_api_provider.dart';
+import '../models/trendingItems.dart';
+
+class Repository {
+  final musicApiProvider = trendingAPIProvider();
+
+  Future<trendingItems> fetchAllMusic() => musicApiProvider.fetchMusicList();
+  Future<lyrics> fetchLyrics(int track_id) => musicApiProvider.fetchLyrics(track_id);
+}
